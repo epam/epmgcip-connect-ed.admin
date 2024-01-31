01182bb3693815207225c25221c62554a0682d22
oleg_saidov <oleg_saidov@epam.com>
2024-01-31 20:39:46 +0500

    Init strapi project, set env vars, seed data, launch locally.

---
+++ config/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => {
+  if (env('NODE_ENV') === 'production') {
+    return {
+      upload: {
+        config: {
+          provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+          providerOptions: {
+            bucketName: env('BUCKET_NAME'),
+            publicFiles: true,
+            uniform: false,
+            basePath: '',
+          },
+        },
+      }
+    }
+  } else {
+    return ({
+      seo: {
+        enabled: true,
+      },
+    })
+  }
+}
+
