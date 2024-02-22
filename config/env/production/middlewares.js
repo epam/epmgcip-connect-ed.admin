18f4cc29c3bdc22ae2821584ad6dbad5ce898138
oleg_saidov <oleg_saidov@epam.com>
2024-02-22 23:05:04 +0500

    Fix for broken image 3

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,5 +1,6 @@
 module.exports = [
   'strapi::errors',
+  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -7,5 +8,29 @@ module.exports = [
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
-  'strapi::public'
+  'strapi::public',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://storage.googleapis.com`
+          ],
+            "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `https://storage.googleapis.com`
+        ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  }
 ];
