09cb9136ddf1df0f8615ad6bbd0f26d80646969d
Maria Veremey <masha.kelbitskaya@gmail.com>
2024-11-26 00:28:44 +0400

    update components

---
+++ src/api/button-theme/routes/button-theme.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * button-theme router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::button-theme.button-theme');
